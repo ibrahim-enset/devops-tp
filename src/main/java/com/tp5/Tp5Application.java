aaa5055243ec8fda50457a2b420c5fa3a07fc2db
ahdadou <ibra.ahdadou@gmail.com>
2025-02-11 21:30:36 +0100

    first commit

---
+++ src/main/java/com/tp5/Tp5Application.java
@@ -0,0 +1,26 @@
+package com.tp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5Application.class, args);
+	}
+
+
+	@RestController
+	class TestController {
+
+		@GetMapping
+		public String test() {
+			return "Hello BDCC";
+		}
+
+	}
+
+}
